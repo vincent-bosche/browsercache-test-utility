7f7292046120e5b85c5aea03a740f93c49d01525
Vincent B <vb@web-gupa.de>
2018-02-27 17:07:06 +0100

    move SSL-certificate path to config.json

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# browsercache-test-utility
+# Browsercache Test Utility
+
+## Requirements
+node & npm
+
+## Installation
+* Run ```bash
+npm install```
+* Create a SSL certificate
+* Copy config_sample.json to config.json. Enter your DB credentials (only if you wish to save results) and the location of certificate-files
+* Run ```bash
+node server.js```
+* Navigate to http://127.0.0.1:8080 or what port you've entered in your config-file.
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const http		= require("http");
 const https		= require("https");
-
-const config	= require(__dirname + "/config.json");
-// SSL Cert
-const cert		= require(__dirname + "/cert.js");
+const fs = require("fs");
 
 const {Callback} 	= require(__dirname + "/Callback.js");
+
+const config	= require(__dirname + "/config.json");
+
+const https_options = {
+    key:  fs.readFileSync(__dirname  + "/" + config.cert.key),
+    cert: fs.readFileSync(__dirname  + "/" + config.cert.cert)
+};
+
 
 // Set process name
 process.title = "Web Cache Controller";
 
 const callback = new Callback();
-const webServerHttps = https.createServer(cert.options, app).listen(config.httpsPort, function() {
+const webServerHttps = https.createServer(https_options, app).listen(config.httpsPort, function() {
 	console.log("HTTPS server on Port " + config.httpsPort);
 });
 const webServer = http.createServer(app).listen(config.httpPort, function() {
